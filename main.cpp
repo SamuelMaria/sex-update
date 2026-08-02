a6ff3ad8f262025170512e1c0e2ac9baee2ffad5
SamuelMaria <287851107+SamuelMaria@users.noreply.github.com>
2026-08-02 15:17:21 -0600

    Created the version control file, we wanna all men get the update in any device, so this checks if you are in the web version (not implemented, yet) or the desktop version. Everyone have to have the right for enyoing the whole experience, no matter which device are they using.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 #include<iostream>
+#include
 
 int main(){
   std::cout << "Hope has become reality" << std::endl;
 }
-
-Anal
 
---
+++ version_control.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+class VersionControl{
+
+    public:
+        int getVersion(){
+
+            int version = 1;
+            
+            return version;
+        }
+
+}
